619047347cee0bc1820ebfc3233888ecf5b62306
yo-1210 <yo.ppy.i3150@gmail.com>
2024-12-23 01:33:54 +0900

    kadai_mdn_jsを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+function getTodayDate() {
+  const today = new Date(); 
+  const year = today.getFullYear(); 
+  const month = today.getMonth() + 1; 
+  const day = today.getDate(); 
+  
+  return `${year}年${month}月${day}日`;
+}
+
+console.log(getTodayDate()); 
